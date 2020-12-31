c748ebeafb356ccc92631bbfdc304f494285dbdc
Gismar <thegismar@gmail.com>
2020-12-31 06:01:28 -0300

    blah because of bleh

--- wenbot/__init__.py
+++ wenbot/__init__.py
@@ -6,7 +6,7 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-    if 'wen' in str(message.content).lower() or 'digg' in str(message.content).lower():
+    if 'wen digg' in str(message.content).lower() or 'when digg' in str(message.content).lower():
         await message.channel.send('soon:tm:')
 
 token = os.getenv('DISCORD')
